c3895fb35be0fdf16325cf688264afae63ac0793
ASHUTOSH RAJ <ashutoshraj0420@gmail.com>
2022-10-21 00:29:05 +0530

    add

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,30 @@
 
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos]=useState(["Ashut","Aman"])
+
+  const [input, setInput]= useState("")
+
+  const addTodo = () =>{
+    setTodos([...todos,input])
+  }
+
   return (
     <div className="App">
-     <h1>Hello!</h1>
+     <h1>Todo app</h1>
+     <input value={input} onChange={event =>setInput(event.target.value)} />
+     <button onClick={addTodo} >Add Todo</button>
+
+     <ul>
+      {todos.map(todo =>{
+        <li>{todo}</li>
+      })
+
+      }
+      
+     </ul>
     </div>
   );
 }
